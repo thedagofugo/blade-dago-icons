f2a09a0bd22cde9ed5ca7b6846bf744cd0e0162e
WillanVelascoFernandez <willanaa07@gmail.com>
2024-08-16 00:10:00 -0400

    Cambios

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,20 @@
 <?php
 
+use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
+// Configura el Finder para localizar los archivos PHP que se deben analizar
 $finder = Finder::create()
-    ->notPath('vendor')
-    ->in(getcwd(). '/src')
-    ->name('*.php')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->notPath('vendor') // Excluye la carpeta vendor
+    ->in(__DIR__ . '/src') // Incluye la carpeta src
+    ->name('*.php') // Analiza solo archivos PHP
+    ->ignoreDotFiles(true) // Ignora archivos ocultos
+    ->ignoreVCS(true); // Ignora archivos en repositorios VCS
 
-return Codeat3\styles($finder);
-
+// Configura las reglas de PHP CS Fixer
+return (new Config())
+    ->setRules([
+        '@PSR2' => true,
+        // Agrega o ajusta las reglas que necesites aquí
+    ])
+    ->setFinder($finder);
