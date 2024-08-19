3eee38c804be8e4de75fecea7a0bbd4ad1439a2b
WillanVelascoFernandez <willanaa07@gmail.com>
2024-08-18 23:04:22 -0400

    Modificacion de workflows y eliminacion de basura

--- config/generation.php
+++ config/generation.php
@@ -1,38 +1,8 @@
 <?php
 
-use Illuminate\Support\Str;
-use Symfony\Component\Finder\SplFileInfo;
-use Codeat3\BladeIconGeneration\IconProcessor;
-
-
-$svgNormalization = static function (string $tempFilepath, array $iconSet, SplFileInfo $file) {
-
-    // perform generic optimizations
-    $iconProcessor = new IconProcessor($tempFilepath, $iconSet, $file);
-    $iconProcessor
-        ->convertStyleToInline()
-        ->optimize()
-        ->postOptimizationAsString(function ($svgLine) {
-            return str_replace([
-                'fill: #090909',
-                'fill:#040602',
-                'fill: #1d1d1d',
-                'fill:#000000',
-                'fill: #1c1c1c',
-                'fill: #000000',
-                'fill: #272425',
-                'fill: #241f20',
-                'fill:#231F20',
-                'fill: #171717',
-                'fill: #2c2c2b',
-            ], 'fill:currentColor', $svgLine);
-        })
-        ->save();
-};
-
 return [
     [ // Regular Outline...
-        'source' => __DIR__.'/../dist/packages/icons/src/svg/32',
+        'source' => __DIR__.'/../node_modules/dago-icons',
         'destination' => __DIR__.'/../resources/svg',
         'safe' => true,
     ],
